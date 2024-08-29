9efcf7ef5ab7b63311f6ecfa4bfa8cb39c3df536
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2024-08-29 19:56:21 -0300

    Funcao Organiza setas, times e Funcao exibeCampLogo + ajustes no css

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const options = {
 async function montaTabela() {
   const endpoint = '/standings'
   const params = {
-    league: 72,
+    league: 71,
     season: 2024
   }
   const searchParams = new URLSearchParams(params)
@@ -19,6 +19,17 @@ async function montaTabela() {
     try {
         const res = await fetch(url + endpoint + '?' + searchParams, options);
         const data = await res.json();
+
+        // Erro de resposta HTTP
+        if (!res.ok) {
+            if (res.status === 404) {
+            throw new Error('Recurso não encontrado (404).');
+            } else if (res.status === 500) {
+            throw new Error('Erro interno do servidor (500).');
+            } else {
+            throw new Error(`Erro HTTP! status: ${res.status}`);
+            }
+        }
 
         const bolinhas = [
             data.response[0].league.standings[0][0].form,
@@ -36,15 +47,42 @@ async function montaTabela() {
             data.response[0].league.standings[0][4].points
         ]
 
+        const times = [
+            [data.response[0].league.standings[0][0].team.name, data.response[0].league.standings[0][0].team.logo],
+            [data.response[0].league.standings[0][1].team.name, data.response[0].league.standings[0][1].team.logo],
+            [data.response[0].league.standings[0][2].team.name, data.response[0].league.standings[0][2].team.logo],
+            [data.response[0].league.standings[0][3].team.name, data.response[0].league.standings[0][3].team.logo],
+            [data.response[0].league.standings[0][4].team.name, data.response[0].league.standings[0][4].team.logo]
+        ]
+
+        const setas = [
+            data.response[0].league.standings[0][0].status,
+            data.response[0].league.standings[0][1].status,
+            data.response[0].league.standings[0][2].status,
+            data.response[0].league.standings[0][3].status,
+            data.response[0].league.standings[0][4].status
+        ]
+
         organizaBolinhas(bolinhas)
         organizaPontos(pontos)
+        organizaTimes(times)
+        organizaSetas(setas)
+        exibeCampLogo(data.response[0].league.logo)
 
-        console.log(data.response[0].league.standings[0][8])
+        console
+
         
     } catch (error) {
-        console.log(error);
-    }
+        // Erro de rede ou de resposta
+        if (error.message.includes('NetworkError')) {
+            console.error('Erro de rede. Verifique sua conexão.');
+          } else {
+            console.error('Erro ao buscar dados da API:', error.message);
+          }
+          return null;
+        }
 }
+
 
 function invertePalavra(palavra) {
     const palavraInvertida = palavra.split('').reverse();
@@ -80,6 +118,47 @@ function organizaPontos(pontos) {
         const posicaoContainer = document.getElementById('posicao-'+t);
         const pontosTd = posicaoContainer.querySelector('td[data-pontos]');
         pontosTd.textContent = pontos[t-1];
+    }
+}
+
+function organizaSetas(setas) {
+    for (let t = 1; t < 6; t++) {
+        const posicaoContainer = document.getElementById('posicao-'+t);
+        const setasTd = posicaoContainer.querySelector('td[data-seta]');
+        const divSeta = setasTd.querySelector('div');
+
+        if(setas[t-1] == "up") {
+            divSeta.className = 'arrow up';
+        } else if (setas[t-1] == "down") {
+            divSeta.className = 'arrow down';
+        } else {
+            divSeta.className = 'square';
+        }
+    }
+}
+
+function organizaTimes(times) {
+    for (let t = 1; t < 6; t++) {
+        const posicaoContainer = document.getElementById('posicao-'+t);
+        const timeTd = posicaoContainer.querySelector('td[data-time]');
+
+        const logoImg = document.createElement('img');
+        logoImg.src = times[t - 1][1];
+        
+        const nomeTime = times[t - 1][0]
+        const nomeTimeNodeText = document.createTextNode(' ' + nomeTime.slice(0,3).toUpperCase());
+        
+        timeTd.appendChild(logoImg);
+        timeTd.appendChild(nomeTimeNodeText);
+    }
+}
+
+function exibeCampLogo(img) {
+    const campLogo = document.querySelector('.camp-logo');
+
+    if (campLogo) {
+      const logoUrl = img;
+      campLogo.src = logoUrl;
     }
 }
 
